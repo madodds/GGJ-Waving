f6d219973fe44cad3dc5adf946d9c839a5679b7d
Matthew Dodds <madodds@gmail.com>
2017-01-21 22:31:27 -0700

    Initial commit
    GGJ 2017

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+	[HideInInspector]
+	public int PlayerOneWaves;
+
+	[HideInInspector]
+	public int PlayerTwoWaves;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PlayerOneWaves = 0;
+		PlayerTwoWaves = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (PlayerOneWaves - PlayerTwoWaves >= 10)
+		{
+
+		}
+		else if (PlayerTwoWaves - PlayerOneWaves >= 10)
+		{
+
+		}
+	}
+}
