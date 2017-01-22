c4f942ea1c4eddc8eceb9dfa61dcfb2f53ca7672
Ellesent <fpaterin@uccs.edu>
2017-01-22 12:18:28 -0700

    Saving changes

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour {
+
+    float fillAmount = 0;
+
+    GameObject score = GameObject.Find("GameOver");
+
+    int pOneWaves;
+    int pTwoWaves;
+
+    float width;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        pOneWaves = score.GetComponent<Score>().PlayerOneWaves;
+
+        pTwoWaves = score.GetComponent<Score>().PlayerTwoWaves;
+
+        width = GetComponent<RectTransform>().rect.width;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (name == "PlayerOneBar")
+        {
+            fillAmount = width / pOneWaves;
+        }
+        else if (name == "PlayerTwoBar")
+        {
+            fillAmount = width / pTwoWaves;
+        }
+		
+	}
+}
